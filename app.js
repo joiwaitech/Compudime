0e65dcb1e9ae5a82812a02710d0b620ac2257c3c
joiwaitech <54688018+joiwaitech@users.noreply.github.com>
2024-03-06 18:01:12 -0700

    Deliveres Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 // Define the endpoint handler for GET /GetURL
 app.get('/GetURL', (req, res) => {
   // Respond with the URL
-  res.send('https://e6ab1fec2ddf461d86e7bb3f2b92d65a.us-central1.gcp.cloud.es.io:9243/app/dashboards#/view/3903cc73-e10b-4660-942e-77c08c2f7d6e');
+  res.send('https://e6ab1fec2ddf461d86e7bb3f2b92d65a.us-central1.gcp.cloud.es.io:9243/app/dashboards?auth_provider_hint=anonymous1#/view/72f756c9-b0c3-4ad1-8445-e385c95135f8?embed=true&_g=(refreshInterval%3A(pause%3A!t%2Cvalue%3A60000)%2Ctime%3A(from%3Anow-1y%2Fd%2Cto%3Anow))&hide-filter-bar=true');
 });
 
 // Start the server on port 3000
